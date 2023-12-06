8410218800834edb0bec25bfc74c0a1f5279298e
morfishman <2023mor.p@ebin.ort.org.il>
2023-12-06 10:42:35 +0200

    add all basic aritmathic operations :) (expecting valid input)

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(operand1: float, operand2: float) -> float:
+    return operand1 + operand2
+
+def sub(operand1: float, operand2: float) -> float:
+    return operand1 - operand2
+
+def mul(operand1: float, operand2: float) -> float:
+    return operand1 * operand2
+
+def div(operand1: float, operand2: float) -> float:
+    return operand1 / operand2
+
+def pow(operand1: float, operand2: float) -> float:
+    return operand1 ** operand2
+
+
+
+
+
