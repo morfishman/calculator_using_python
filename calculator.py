b702288f9c628c86d7a7f40110382d0af2f26a15
morfishman <2023mor.p@ebin.ort.org.il>
2023-12-06 10:52:46 +0200

    error massage generator function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import json
+
 def add(operand1: float, operand2: float) -> float:
     return operand1 + operand2
 
@@ -13,7 +15,16 @@ def div(operand1: float, operand2: float) -> float:
 def pow(operand1: float, operand2: float) -> float:
     return operand1 ** operand2
 
-
-
+def read_error_messages(file_path,error_type,where):
+    try:
+        with open(file_path, 'r') as file:
+            error_data = json.load(file)
+            return error_data.get(error_type)[where]
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return None
+    except:
+        print(f"Error: erorr not found at {error_type} -> {where}")
+        return None
 
 
