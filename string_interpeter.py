4fd55088dff5eda12b6e4a317cacc4d3ccbc4d17
morfishman <2023mor.p@ebin.ort.org.il>
2023-12-07 17:38:52 +0200

    added calc function that uses the output of input validation and calc the result

--- string_interpeter.py
+++ string_interpeter.py
@@ -1,6 +1,12 @@
-from calculator import read_error_messages
+from calculator import *
 
-def input_check(expression :str) -> bool:
+
+def first_index(string : list,charecture :str)-> int:
+    for char in range(len(string)):
+        if string[char] == charecture: return char
+    return -1
+
+def input_check(expression :str) -> list:
     expression = expression.replace(' ','')
     laxers_array = []
     buffer = ''
@@ -14,24 +20,70 @@ def input_check(expression :str) -> bool:
                 laxers_array.append(laxer)
             else:
                 print(f"in operand {buffer}\n", read_error_messages(file_path='', error_type='input errors', where='operand'))
-                return False
+                return []
         else:
             print(f"in syntax {laxer}\n",read_error_messages(file_path='Errors.json', error_type='type errors', where='operator'))
-            return False
+            return []
     if buffer != '':
         if buffer.isdigit():
             laxers_array.append(buffer)
             buffer = ''
         else:
             print(f"in operand {buffer}\n",read_error_messages(file_path='Errors.json', error_type='input errors', where='operand'))
-            return False
+            return []
     if laxers_array == [] or laxers_array[len(laxers_array)-1] in ['+','-','/','^','*']:
         print(f"in syntax {laxer}\n",read_error_messages(file_path='Errors.json', error_type='input errors', where='operator'))
-        return False
-    return True
+        return []
+    return laxers_array
+
+def calc_expreation(expression :list) -> float:
+    if len(expression) == 1:
+        return float(expression[0])
+    else:
+        div_index = first_index(expression,'/')
+        mul_index = first_index(expression,'*')
+        add_index = first_index(expression,'+')
+        sub_index = first_index(expression,'-')
+        useble_index = None
+        func = None
+        if mul_index != -1 and div_index  != -1:
+            if mul_index> div_index:
+                useble_index = div_index
+                func = div
+            else:
+                useble_index = mul_index
+                func = mul
+        elif mul_index != -1 or div_index  != -1:
+            if mul_index == -1:
+                useble_index = div_index
+                func = div
+            else:
+                useble_index = mul_index
+                func = mul
+
+        elif add_index != -1 and sub_index != -1:
+            if add_index > sub_index:
+                useble_index = sub_index
+                func = sub
+            else:
+                useble_index = add_index
+                func = add
+        else:
+            if add_index == -1:
+                useble_index = sub_index
+                func = sub
+            else:
+                useble_index = add_index
+                func = add
 
 
+        operator = expression.pop(useble_index)
+        operand2 = expression.pop(useble_index)
+        operand1 = expression[useble_index - 1]
+        expression[useble_index - 1] = gerneral_aritmatic(operand1=float(operand1), operand2=float(operand2),operator=operator, function_aritmatic= func)
+        if expression[useble_index - 1] == None:
+            return
 
-
+        return calc_expreation(expression)
 
 
